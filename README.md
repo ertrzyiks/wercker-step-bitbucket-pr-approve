1e143e14173b9fd83168d6058e97389a1ce2a10d
Mateusz Derks <mateusz.derks@schibsted.pl>
2015-04-19 14:18:03 +0200

    Implement approve/disapprove pr

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# wercker-find-bitbucket-pull-request-id
+# wercker-approve-bitbucket-pull-request
+Approve [bitbucket](http://bitbucket.com) pull request or commit on successful build. Remove approval on failed build.
+
+Based on https://github.com/hyzhak/wercker-step-bitbucket-approve
+
+### Required fields
+
+* `username` - user name of account which will be wercker bot and will approve commit. (for example: '<owner>-wercker').
+* `password` - password of bot user.
+
+### How to configure?
+
+You should create additional account for wercker bot. And give him permition for reading target repo.
+
+# Example
+
+    build:
+        after-steps:
+            - ertrzyiks/bitbucket-pr-approve:
+                username: my-application-wercker
+                password: $WERCKER_BITBUCKET_USER_PASSWORD
+
+# License
+
+The MIT License (MIT)
+
+Copyright (c) 2015 Mateusz Derks
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+# Changelog
+
+## 0.0.2
+- initial version
--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-if [ ! -n "$WERCKER_BITBUCKET_APPROVE_PASSWORD" ]; then
+if [ ! -n "$WERCKER_BITBUCKET_PR_APPROVE_PASSWORD" ]; then
   fail 'Missing password property'
 fi
 
-if [ ! -n "$WERCKER_BITBUCKET_APPROVE_USERNAME" ]; then
+if [ ! -n "$WERCKER_BITBUCKET_PR_APPROVE_USERNAME" ]; then
   fail 'Missing username property'
 fi
 
@@ -11,5 +11,75 @@ if [ -n "$DEPLOY" ]; then
   fail 'Should be used for build steps'
 fi
 
-RESULT=`curl -u $WERCKER_BITBUCKET_APPROVE_USERNAME:$WERCKER_BITBUCKET_APPROVE_PASSWORD -s "https://api.bitbucket.org/2.0/repositories/$WERCKER_GIT_OWNER/$WERCKER_GIT_REPOSITORY/pullrequests?pagelen=50"  --output $WERCKER_STEP_TEMP/result.txt -w "%{http_code}"`
-echo $(cat result.txt | $WERCKER_STEP_ROOT/bin/jq ".values[] | { id, hash: .source.commit.hash } | select(.hash == \"$WERCKER_GIT_COMMIT\")");
+function verifyResult {
+  if [ "$RESULT" = "401" ]; then
+    fail "Invalid username or password"
+  fi
+
+  if [ "$RESULT" = "404" ]; then
+    fail "Subdomain or token not found."
+  fi
+
+  if [ "$RESULT" = "500" ]; then
+    if grep -Fqx "No token" $WERCKER_STEP_TEMP/result.txt; then
+      fail "No token is specified."
+    fi
+
+    if grep -Fqx "No hooks" $WERCKER_STEP_TEMP/result.txt; then
+      fail "No hook can be found for specified subdomain/token"
+    fi
+
+    if grep -Fqx "Invalid channel specified" $WERCKER_STEP_TEMP/result.txt; then
+      fail "Could not find specified channel for subdomain/token."
+    fi
+
+    if grep -Fqx "No text specified" $WERCKER_STEP_TEMP/result.txt; then
+      fail "No text specified."
+    fi
+
+    fail "Unknown error."
+  fi
+}
+
+function doGetRequest {
+  RESULT_CODE=`curl -u $WERCKER_BITBUCKET_PR_APPROVE_USERNAME:$WERCKER_BITBUCKET_PR_APPROVE_PASSWORD -s $1 --output $WERCKER_STEP_TEMP/result.txt -w "%{http_code}"`
+  RESULT_BODY=$(cat $WERCKER_STEP_TEMP/result.txt);
+
+  verifyResult
+}
+
+function doPostRequest {
+  RESULT_CODE=`curl -d "" -u $WERCKER_BITBUCKET_PR_APPROVE_USERNAME:$WERCKER_BITBUCKET_PR_APPROVE_PASSWORD -s $1 --output $WERCKER_STEP_TEMP/result.txt -w "%{http_code}"`
+  RESULT_BODY=$(cat $WERCKER_STEP_TEMP/result.txt);
+
+  verifyResult
+}
+
+function doDeleteRequest {
+  RESULT_CODE=`curl -X DELETE -u $WERCKER_BITBUCKET_PR_APPROVE_USERNAME:$WERCKER_BITBUCKET_PR_APPROVE_PASSWORD -s $1 --output $WERCKER_STEP_TEMP/result.txt -w "%{http_code}"`
+  RESULT_BODY=$(cat $WERCKER_STEP_TEMP/result.txt);
+
+  verifyResult
+}
+
+doGetRequest "https://api.bitbucket.org/2.0/repositories/$WERCKER_GIT_OWNER/$WERCKER_GIT_REPOSITORY/pullrequests?pagelen=50"
+PULL_REQUEST_ID=$(echo $RESULT_BODY | $WERCKER_STEP_ROOT/bin/jq ".values[] | { id, hash: .source.commit.hash } | select(.hash == \"${WERCKER_GIT_COMMIT:0:12}\")| .id")
+echo "Pull request: # $PULL_REQUEST_ID";
+
+if [ "$WERCKER_RESULT" = "passed" ]; then
+  if [ -z "$PULL_REQUEST_ID" ]; then
+    echo "Pull request not found, approve commit $WERCKER_GIT_COMMIT";
+    doPostRequest "https://api.bitbucket.org/2.0/repositories/$WERCKER_GIT_OWNER/$WERCKER_GIT_REPOSITORY/commit/$WERCKER_GIT_COMMIT/approve"
+  else
+    echo "Approve pull request $PULL_REQUEST_ID";
+    doPostRequest "https://api.bitbucket.org/2.0/repositories/$WERCKER_GIT_OWNER/$WERCKER_GIT_REPOSITORY/pullrequests/$PULL_REQUEST_ID/approve"
+  fi
+else
+  if [ -z "$PULL_REQUEST_ID" ]; then
+    echo "Pull request not found, disapprove commit $WERCKER_GIT_COMMIT";
+    doDeleteRequest "https://api.bitbucket.org/2.0/repositories/$WERCKER_GIT_OWNER/$WERCKER_GIT_REPOSITORY/commit/$WERCKER_GIT_COMMIT/approve"
+  else
+    echo "Disapprove pull request $PULL_REQUEST_ID";
+    doDeleteRequest "https://api.bitbucket.org/2.0/repositories/$WERCKER_GIT_OWNER/$WERCKER_GIT_REPOSITORY/pullrequests/$PULL_REQUEST_ID/approve"
+  fi
+fi
